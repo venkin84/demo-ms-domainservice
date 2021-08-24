4c9cc2f4677efb37cb8012d8d16eb3e94500b0ef
venkin84 <venkataraman.n.mail@gmail.com>
2021-08-24 09:25:58 +0530

    Initial Commit

---
+++ src/main/java/com/venkin84/demomsdomainservice/DemoMsDomainserviceApplication.java
@@ -0,0 +1,13 @@
+package com.venkin84.demomsdomainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMsDomainserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMsDomainserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/venkin84/demomsdomainservice/DemoMsDomainserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkin84.demomsdomainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMsDomainserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
